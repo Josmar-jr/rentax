4359a31c82cd40e8c4e4c4d532f1ea969b78066f
josmar.junior.128 <josmar.junior.128@ufrn.edu.br>
2021-12-31 14:08:53 -0300

    create specifications

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,35 @@
+import { Specification } from "../model/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationRepository implements ISpecificationRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO) {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string) {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+
+    return specification;
+  }
+}
+
+export { SpecificationRepository };
--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,5 +1,28 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {}
+  constructor(private specificationRepository: ISpecificationRepository) {}
+
+  execute({ name, description }: IRequest) {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error(`Specification ${name} already exists!`);
+    }
+
+    this.specificationRepository.create({
+      name,
+      description,
+    });
+
+    return;
+  }
 }
 
 export { CreateSpecificationService };
