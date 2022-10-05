6e16be536e102251f46c0d62aeabb34fe3cd6d2e
josmar-jr <josmartrigueiro.jr@gmail.com>
2022-10-05 12:38:41 -0300

    feat(importCategories): create useCase from import categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,30 @@
+import path from "node:path";
 import { Router } from "express";
+import multer from "multer";
+import { format } from "date-fns";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { importCategoriesController } from "../modules/cars/useCases/importCategories";
 
 const categoriesRoutes = Router();
+
+const DESTINATION_FOLDER = path.resolve(__dirname, "../../tmp");
+
+const upload = multer.diskStorage({
+  destination: DESTINATION_FOLDER,
+  filename: (request, file, callback) => {
+    const formatDate = format(new Date(), "dd-MM-yyyy");
+    const fileName = `${formatDate}_${file.originalname}`;
+
+    return callback(null, fileName);
+  },
+});
+
+const uploadFile = multer({
+  dest: DESTINATION_FOLDER,
+  storage: upload,
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -12,5 +33,13 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post(
+  "/upload",
+  uploadFile.single("file"),
+  (request, response) => {
+    return importCategoriesController.handle(request, response);
+  }
+);
 
 export { categoriesRoutes };
