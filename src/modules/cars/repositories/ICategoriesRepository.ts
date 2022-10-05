d830b8a8494c9af55ea46064689598907a3eb29d
josmar-jr <josmartrigueiro.jr@gmail.com>
2022-10-04 23:38:09 -0300

    feat: add model category and specification

---
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -0,0 +1,10 @@
+import { Category } from "../model/Category";
+import { ICreateCategoryDTO } from "./CategoriesRepository";
+
+interface ICategoriesRepository {
+  create({name, description}: ICreateCategoryDTO): void;
+  list(): Category[];
+  findByName(name: string): Category;
+}
+
+export { ICategoriesRepository }
